a256c4fd0e5ee146913282cc60a1ecc4e8a98b63
bigegi84 <gilangpratamaw@gmail.com>
2021-11-08 20:09:12 +0700

    up

--- task3-folder/src/helper/http/user/createOne.js
+++ task3-folder/src/helper/http/user/createOne.js
@@ -4,21 +4,27 @@ module.exports = () => {
     try {
       if (req.body.name == null) {
         res.status(422).send({ status: "fail", message: "name undefined" });
+        throw "error";
       }
       if (typeof req.body.name != "string") {
         res.status(422).send({ status: "fail", message: "name not string" });
+        throw "error";
       }
       if (req.body.name == "") {
         res.status(422).send({ status: "fail", message: "name is empty" });
+        throw "error";
       }
       if (req.body.email == null) {
         res.status(422).send({ status: "fail", message: "email undefined" });
+        throw "error";
       }
       if (typeof req.body.email != "string") {
         res.status(422).send({ status: "fail", message: "email not string" });
+        throw "error";
       }
       if (req.body.email == "") {
         res.status(422).send({ status: "fail", message: "email is empty" });
+        throw "error";
       }
       const emailFound = await model.user.findOne({
         where: { email: req.body.email },
@@ -27,6 +33,15 @@ module.exports = () => {
         res
           .status(422)
           .send({ status: "fail", message: "email already registered" });
+        throw "error";
+      }
+      if (req.body.age != null) {
+        if (typeof req.body.age != "number") {
+          res
+            .status(422)
+            .send({ status: "fail", message: "email not integer" });
+          throw "error";
+        }
       }
       const data = await model.user.create(req.body);
       res.send({ status: "ok", data });
